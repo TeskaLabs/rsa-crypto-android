2bd4bd29428b81f7a809cde229ff9ba12e1e0c74
Ales Teska <ales.teska@teskalabs.com>
2018-05-04 09:07:56 +0200

    Initial import.

---
+++ app/src/main/java/com/teskalabs/ateska/rsakeypaircrypto/MainActivity.java
@@ -0,0 +1,279 @@
+package com.teskalabs.ateska.rsakeypaircrypto;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.security.GeneralSecurityException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.crypto.spec.SecretKeySpec;
+
+public class MainActivity extends AppCompatActivity {
+
+	private static final String TAG = MainActivity.class.getName();
+
+	private RSAKeyPair rsaKeyPair;
+
+	private TextView keyStatusTextView = null;
+	private EditText inputText = null;
+	private TextView encryptedTextView = null;
+	private Spinner keysizeSpinner = null;
+
+	private ProgressBar progressBar = null;
+	int busy = 0;
+
+	private final ExecutorService es = Executors.newSingleThreadExecutor();
+
+	private static final int REQUEST_CODE_USER_AUTHORIZED = 1;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		keyStatusTextView = (TextView) findViewById(R.id.keyStatusTextView);
+		inputText = (EditText) findViewById(R.id.inputText);
+		encryptedTextView = (TextView) findViewById(R.id.encryptedTextView);
+		progressBar = (ProgressBar) findViewById(R.id.progressBar);
+		keysizeSpinner = (Spinner) findViewById(R.id.keysizeSpinner);
+		keysizeSpinner.setSelection(3); // Default is 2048 RSA key length
+
+		PRNGFixes.apply();
+
+		rsaKeyPair = new RSAKeyPair("RSAKeyPair");
+
+		updateStatus();
+	}
+
+	public void onGenerateKeyPair(View v) {
+
+		try {
+			rsaKeyPair.discard();
+		} catch (GeneralSecurityException e) {
+			Log.w(TAG, "Failed to discard a key");
+		}
+		updateStatus();
+
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.MILLISECOND, 0);
+		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.HOUR_OF_DAY, 0);
+		final Date valid_from = cal.getTime();
+		cal.add(Calendar.YEAR, 200);
+		final Date valid_to = cal.getTime();
+		final Integer keySize = Integer.parseInt(keysizeSpinner.getSelectedItem().toString());
+
+		UserAuth.unlockCredentials(this);
+
+		es.submit(new Runnable() {
+			@Override
+			public void run() {
+				MainActivity.this.setBusy(true);
+				try
+				{
+					try {
+						rsaKeyPair.generate(MainActivity.this, keySize, "CN=test1/O=TeskaLabs Ltd", valid_from, valid_to, 1, true);
+					} catch (Exception e) {
+						Log.e(TAG, "RSAKeyPair", e);
+						makeToast("RSA key pair generation failed :-(");
+					}
+					updateStatus();
+				}
+				finally {
+					MainActivity.this.setBusy(false);
+				}
+			}
+		});
+	}
+
+	public void onDeleteKeyPair(View v) {
+		try {
+			rsaKeyPair.discard();
+		} catch (GeneralSecurityException e) {
+			Log.e(TAG, "Failed to discard a key");
+			makeToast("Failed to discard a kay");
+		}
+		updateStatus();
+	}
+
+	public void onEncrypt(View v) {
+
+		final byte[] input = inputText.getText().toString().getBytes();
+
+		es.submit(new Runnable() {
+			@Override
+			public void run() {
+				MainActivity.this.setBusy(true);
+				try
+				{
+					byte[] output = rsaKeyPair.encrypt(input);
+					updateStatus();
+
+					if (output == null)
+					{
+						makeToast("Failed to encrypt!");
+						return;
+					}
+
+					final String encodedOutput = Base64.encodeToString(output, Base64.DEFAULT);
+					runOnUiThread(new Runnable() {
+						public void run() {
+							encryptedTextView.setText(encodedOutput);
+						}
+					});
+				}
+
+				catch (GeneralSecurityException e) {
+					Log.e(TAG, "Error when encrypting");
+					makeToast("Failed to encrypt!");
+				}
+
+				finally {
+					MainActivity.this.setBusy(false);
+				}
+			}
+		});
+	}
+
+	public void onDecrypt(View v) {
+		String encodedInput = encryptedTextView.getText().toString();
+		final byte[] input = Base64.decode(encodedInput, Base64.DEFAULT);
+
+		es.submit(new Runnable() {
+			@Override
+			public void run() {
+				MainActivity.this.setBusy(true);
+				try {
+					byte[] output = rsaKeyPair.decrypt(input);
+					if (output == null) {
+						makeToast("Decrypt failed!");
+						return;
+					}
+					final String outputString = new String(output);
+					makeToast(outputString);
+				}
+				catch (UserAuth.UserNotAuthenticatedException e) {
+					UserAuth.showAuthenticationScreen(MainActivity.this, null, null, REQUEST_CODE_USER_AUTHORIZED);
+				}
+				catch (GeneralSecurityException e) {
+					Log.e(TAG, "Error when decrypting");
+					makeToast("Decrypt failed!");
+				}
+				finally {
+					MainActivity.this.setBusy(false);
+				}
+			}
+		});
+	}
+
+
+	public void onDeriveKey(View view) {
+		es.submit(new Runnable() {
+			@Override
+			public void run() {
+				MainActivity.this.setBusy(true);
+				try {
+					byte[] key = rsaKeyPair.derive("test-id", 32);
+					SecretKeySpec secretkey = new SecretKeySpec(key, "AES");
+
+					final String encodedOutput = Base64.encodeToString(key, Base64.DEFAULT);
+					runOnUiThread(new Runnable() {
+						public void run() {
+							encryptedTextView.setText(encodedOutput);
+						}
+					});
+				}
+				catch (UserAuth.UserNotAuthenticatedException e) {
+					UserAuth.showAuthenticationScreen(MainActivity.this, null, null, REQUEST_CODE_USER_AUTHORIZED);
+				}
+				catch (GeneralSecurityException e) {
+					Log.e(TAG, "Error when deriving key");
+					makeToast("Key derivation failed!");
+				}
+				catch (Exception e) {
+					Log.e(TAG, "Error when deriving key", e);
+					makeToast("Key derivation failed!");
+				}
+				finally {
+					MainActivity.this.setBusy(false);
+				}
+			}
+		});
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		if (requestCode == REQUEST_CODE_USER_AUTHORIZED)
+		{
+			if (resultCode == RESULT_OK) {
+				onDecrypt(null);
+			}
+		}
+	}
+
+	protected void makeToast(final String text) {
+		runOnUiThread(new Runnable() {
+			public void run() {
+				Toast.makeText(MainActivity.this, text, Toast.LENGTH_LONG).show();
+			}
+		});
+	}
+
+	synchronized protected void setBusy(boolean isBusy)
+	{
+		if (isBusy)
+		{
+			busy += 1;
+			if (busy == 1) runOnUiThread(new Runnable() {
+				public void run() {
+					progressBar.setIndeterminate(true);
+				}
+			});
+		}
+		else
+		{
+			busy -= 1;
+			if (busy < 0) busy = 0;
+			if (busy == 0) runOnUiThread(new Runnable() {
+				public void run() {
+					progressBar.setIndeterminate(false);
+				}
+			});
+
+		}
+	}
+
+	protected void updateStatus()
+	{
+		runOnUiThread(new Runnable() {
+			public void run() {
+				try {
+					if (rsaKeyPair.exists()) {
+                        keyStatusTextView.setBackgroundColor(Color.parseColor("#00FF00"));
+                    } else {
+                        keyStatusTextView.setBackgroundColor(Color.parseColor("#FF0000"));
+                    }
+				} catch (GeneralSecurityException e) {
+					Log.e(TAG, "rsaKeyPair.exists() failed");
+					keyStatusTextView.setBackgroundColor(Color.parseColor("#00FFFF"));
+				}
+			}
+		});
+	}
+
+}
---
+++ app/src/main/java/com/teskalabs/ateska/rsakeypaircrypto/RSAKeyPair.java
@@ -0,0 +1,308 @@
+package com.teskalabs.ateska.rsakeypaircrypto;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.security.KeyPairGeneratorSpec;
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyProperties;
+import android.security.keystore.UserNotAuthenticatedException;
+import android.util.Log;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.security.GeneralSecurityException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.cert.Certificate;
+import java.security.interfaces.RSAPrivateKey;
+import java.util.Date;
+
+import javax.crypto.Cipher;
+import javax.crypto.Mac;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import javax.security.auth.x500.X500Principal;
+
+
+public class RSAKeyPair {
+
+    private static final String TAG = RSAKeyPair.class.getName();
+    private final String alias;
+
+
+    public RSAKeyPair(String alias) {
+        this.alias = alias;
+    }
+
+
+    public boolean exists() throws GeneralSecurityException {
+        KeyStore keyStore = obtainKeyStore();
+        PrivateKey privateKey = (PrivateKey) keyStore.getKey(alias, null);
+        if (privateKey == null) return false;
+        return true;
+    }
+
+
+    public void discard() throws GeneralSecurityException {
+        KeyStore keyStore = obtainKeyStore();
+        keyStore.deleteEntry(alias);
+    }
+
+
+    public byte[] decrypt(byte[] input) throws UserAuth.UserNotAuthenticatedException, GeneralSecurityException {
+        KeyStore keyStore = obtainKeyStore();
+        PrivateKey privateKey = (PrivateKey) keyStore.getKey(alias, null);
+        if (privateKey == null) return null; // No key pair
+
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        try {
+            return cipher.doFinal(input);
+        } catch (GeneralSecurityException e) {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (e instanceof UserNotAuthenticatedException)
+                {
+                    Log.w(TAG, "User not logged in!");
+                    throw new UserAuth.UserNotAuthenticatedException(e);
+                }
+            }
+
+            throw e;
+        }
+    }
+
+
+    public byte[] encrypt(byte[] input) throws GeneralSecurityException {
+        KeyStore keyStore = obtainKeyStore();
+        PublicKey publicKey = keyStore.getCertificate(alias).getPublicKey();
+        if (publicKey == null) return null; // No key pair
+
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        return cipher.doFinal(input);
+    }
+
+
+    public byte[] derive(String keyId, int outLengthBytes) throws UserAuth.UserNotAuthenticatedException, GeneralSecurityException {
+		// More at https://en.wikipedia.org/wiki/HKDF
+    	// Inspired by https://github.com/patrickfav/hkdf
+
+		KeyStore keyStore = obtainKeyStore();
+
+		RSAPrivateKey privateKey = (RSAPrivateKey) keyStore.getKey(alias, null);
+		if (privateKey == null) return null; // No key pair
+
+		int ikmLenght = privateKey.getModulus().bitLength() / 8;
+		byte[] nounce = new byte[ikmLenght];
+
+		for (int i=0; i<ikmLenght; i+=1)
+		{
+			nounce[i] = 0x77;
+		}
+
+		Cipher cipher = Cipher.getInstance("RSA/ECB/NoPadding");
+		cipher.init(Cipher.DECRYPT_MODE, privateKey);
+
+		byte[] ikm; // input Keying Material
+		try {
+			ikm = cipher.doFinal(nounce);
+		} catch (GeneralSecurityException e) {
+
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+				if (e instanceof UserNotAuthenticatedException)
+				{
+					Log.w(TAG, "User not logged in!");
+					throw new UserAuth.UserNotAuthenticatedException(e);
+				}
+			}
+
+			throw e;
+		}
+
+		Mac HMAC = Mac.getInstance("HmacSHA256");
+		SecretKey salt = new SecretKeySpec(new byte[HMAC.getMacLength()], "HmacSHA256");
+		HMAC.init(salt);
+		SecretKey prk = new SecretKeySpec(HMAC.doFinal(ikm),"HmacSHA256");
+		for (int i=0; i<ikmLenght; i+=1)
+		{
+			nounce[i] = 0x77;
+		}
+
+		byte[] t = new byte[0];
+
+		int iterations = (int) Math.ceil(((double) outLengthBytes) / ((double) HMAC.getMacLength()));
+
+		if (iterations > 255) {
+			throw new IllegalArgumentException("out length must be maximal 255 * hash-length; requested: " + outLengthBytes + " bytes");
+		}
+
+		ByteBuffer buffer = ByteBuffer.allocate(outLengthBytes);
+		int remainingBytes = outLengthBytes;
+		int stepSize;
+
+		for (int i = 0; i < iterations; i++) {
+			HMAC = Mac.getInstance("HmacSHA256");
+			HMAC.init(prk);
+
+			HMAC.update(t);
+			HMAC.update(keyId.getBytes());
+			HMAC.update((byte) (i + 1));
+
+			t = HMAC.doFinal();
+
+			stepSize = Math.min(remainingBytes, t.length);
+
+			buffer.put(t, 0, stepSize);
+			remainingBytes -= stepSize;
+		}
+
+		return buffer.array();
+	}
+
+
+    public void generate(Context context, int keySize, String subject, Date valid_from, Date valid_to, int serialNumber, boolean requireUserAuth) throws GeneralSecurityException {
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR2) {
+			generate_api17(context, keySize, subject, valid_from, valid_to, serialNumber, requireUserAuth);
+        }
+        else if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            generate_api18_22(context, keySize, subject, valid_from, valid_to, serialNumber, requireUserAuth);
+        }
+        else {
+            generate_api23(keySize, subject, valid_from, valid_to, serialNumber, requireUserAuth);
+        }
+
+
+    }
+
+	private void generate_api17(Context context, int keySize, String subject, Date valid_from, Date valid_to, int serialNumber, boolean requireUserAuth) throws GeneralSecurityException {
+
+		X500Principal subjectPrincipal = new X500Principal(subject);
+		KeyStore keyStore = obtainKeyStore();
+
+		KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA", "BC");
+		kpg.initialize(keySize);
+		KeyPair keyPair = kpg.genKeyPair();
+
+		//TODO: This code is not completed: how to generate self-signed certificate on Android <=17?
+	}
+
+	@TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+    private void generate_api18_22(Context context, int keySize, String subject, Date valid_from, Date valid_to, int serialNumber, boolean requireUserAuth) throws GeneralSecurityException {
+
+        Context applicationContext = context.getApplicationContext();
+        X500Principal subjectPrincipal = new X500Principal(subject);
+
+        KeyStore keyStore = obtainKeyStore();
+
+        KeyPairGeneratorSpec.Builder builder = new KeyPairGeneratorSpec.Builder(applicationContext);
+        builder.setAlias(alias);
+        builder.setStartDate(valid_from);
+        builder.setEndDate(valid_to);
+        builder.setSerialNumber(BigInteger.valueOf(serialNumber));
+        builder.setSubject(subjectPrincipal);
+
+        if (requireUserAuth) {
+            builder.setEncryptionRequired();
+        }
+
+        // API level 18 has RSA key fixed to 2048
+        // See http://stackoverflow.com/questions/39998087/how-to-set-key-size-for-keypairgeneratorspec-on-api-18
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            builder.setKeySize(keySize);
+        }
+
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA", "AndroidKeyStore");
+        kpg.initialize(builder.build());
+
+        KeyPair kp = kpg.generateKeyPair();
+
+        PrivateKey privateKey = (PrivateKey) keyStore.getKey(alias, null);
+        if (privateKey == null)
+            throw new SecurityException("Failed to obtain private key from a generated key pair");
+
+        PublicKey publicKey = keyStore.getCertificate(alias).getPublicKey();
+        if (publicKey == null)
+            throw new SecurityException("Failed to obtain private key from a generated key pair");
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void generate_api23(int keySize, String subject, Date valid_from, Date valid_to, int serialNumber, boolean requireUserAuth) throws GeneralSecurityException {
+
+        X500Principal subjectPrincipal = new X500Principal(subject);
+
+        KeyStore keyStore = obtainKeyStore();
+
+        KeyGenParameterSpec.Builder builder = new KeyGenParameterSpec.Builder(alias, KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT);
+        builder.setKeySize(keySize);
+        builder.setBlockModes(KeyProperties.BLOCK_MODE_ECB); // For backward compatibility
+        builder.setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_RSA_PKCS1); // For backward compatibility
+
+        builder.setCertificateNotBefore(valid_from);
+        builder.setCertificateNotAfter(valid_to);
+        builder.setCertificateSerialNumber(BigInteger.valueOf(serialNumber));
+        builder.setCertificateSubject(subjectPrincipal);
+
+        if (requireUserAuth) {
+            builder.setUserAuthenticationRequired(true);
+            builder.setUserAuthenticationValidityDurationSeconds(5 * 60);
+        }
+
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance(KeyProperties.KEY_ALGORITHM_RSA, "AndroidKeyStore");
+
+        kpg.initialize(builder.build());
+
+        KeyPair kp = kpg.generateKeyPair();
+
+        PrivateKey privateKey = (PrivateKey) keyStore.getKey(alias, null);
+        if (privateKey == null)
+            throw new SecurityException("Failed to obtain private key from a generated key pait");
+
+        PublicKey publicKey = keyStore.getCertificate(alias).getPublicKey();
+        if (publicKey == null)
+            throw new SecurityException("Failed to obtain private key from a generated key pait");
+
+        // Following code works only on API 23+
+        //KeyFactory kf = KeyFactory.getInstance(privateKey.getAlgorithm(), "AndroidKeyStore");
+        //KeyInfo keyInfo = kf.getKeySpec(privateKey, KeyInfo.class);
+        //Log.i(TAG, "Pair: " + privateKey + " / " + publicKey + " isInHardware:" + keyInfo.isInsideSecureHardware());
+    }
+
+
+    private KeyStore obtainKeyStore() throws GeneralSecurityException {
+		KeyStore keyStore;
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR2) {
+			keyStore = KeyStore.getInstance("BKS");
+		}
+		else {
+			keyStore = KeyStore.getInstance("AndroidKeyStore");
+			try {
+				keyStore.load(null);
+			} catch (IOException e) {
+				throw new KeyStoreException("Key store error", e);
+			}
+		}
+        return keyStore;
+    }
+
+
+    public Certificate getCertificate() throws GeneralSecurityException {
+        KeyStore keyStore = obtainKeyStore();
+        if (keyStore == null) return null;
+        return keyStore.getCertificate(alias);
+    }
+
+    public PublicKey getPublicKey() throws GeneralSecurityException {
+        Certificate cert = getCertificate();
+        if (cert == null) return null;
+        return cert.getPublicKey();
+    }
+
+}
